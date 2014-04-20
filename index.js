f486386fd08f475df1bebb90c368adf8abbe6910
Samy Pessé <samypesse@gmail.com>
2014-04-20 15:16:07 +0200

    Use mathjax from cdn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
     book: {
         assets: "./book",
         js: [
-            "mathjax.js",
+            "http://cdn.mathjax.org/mathjax/2.0-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
             "plugin.js"
         ]
     }
