6df21fb86a5c97a933a1b6ef256feb399dbad714
Samy Pessé <samypesse@gmail.com>
2015-02-12 08:25:16 +0100

    Update website mathjax as 2.5

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = {
     book: {
         assets: "./book",
         js: [
-            "https://cdn.mathjax.org/mathjax/2.4-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
+            "https://cdn.mathjax.org/mathjax/2.5-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
             "plugin.js"
         ]
     },
