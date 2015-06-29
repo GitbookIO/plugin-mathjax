1bc7ac21fbd66d70c476b53f3a61a722ea185bd5
Samy Pesse <samypesse@gmail.com>
2015-06-29 10:04:35 -0400

    Fix GitbookIO/gitbook#778: support shortcuts in asciidoc and rst

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ module.exports = {
     blocks: {
         math: {
             shortcuts: {
-                parsers: ["markdown"],
+                parsers: ["markdown", "asciidoc", "restructuredtext"],
                 start: "$$",
                 end: "$$"
             },
